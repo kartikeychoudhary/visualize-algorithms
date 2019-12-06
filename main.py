f70540cfb5f809fcc98bfc8f8a2c699c1eb3e50d
Kartikey Choudhary <31shavy@gmail.com>
2019-12-06 22:03:31 +0530

    Files Added

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask , render_template, request, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+import requests
+import flask
+from algorithms.bubblesort import BubbleSort
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/bubblesort', methods=['POST'])
+def bubblesort():
+    json = request.get_json()
+    array = json['array']
+    obj = BubbleSort(array)
+    json = obj.sortSteps()
+    return jsonify(json)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/bubblesort.js
@@ -0,0 +1,5 @@
+var Array_Steps = []; // arrays which holds the details from the swaps of bubble sort
+var Array_Swap = [];
+var Array_Index = [];
+
+var url = "http://127.0.0.1:5000/bubblesort"
